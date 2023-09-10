3f33ea999c26039539ff202c04c9aa23439f847b
albertfrancajosuacosta <albertfrancajosuacosta@gmail.com>
2023-09-09 22:51:21 -0300

    Create classes __init__.py and base.py.

---
+++ __init__.py
@@ -0,0 +1,8 @@
+""" 
+Active Learning 18
+
+@author: Albert França Josuá Costa
+@email: albertfrancajosuacosta@gmail.com
+"""
+
+from .import base
---
+++ base.py
@@ -0,0 +1,16 @@
+""" 
+Active Learning 18
+
+@author: Albert França Josuá Costa
+@email: albertfrancajosuacosta@gmail.com
+"""
+
+import abc
+
+class ActiveLearningBase():
+    
+    """ Base classe for Active Learning Library """
+    
+    @abc.abstractmethod()
+    def describe(self, describe):
+        ...
